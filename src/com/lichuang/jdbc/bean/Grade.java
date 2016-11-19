6a4aec72fb2085e4430348cdbda6f882c5484568
li-chuang <278747148@qq.com>
2016-11-19 23:48:15 +0800

    Create Grade.java

---
+++ src/com/lichuang/jdbc/bean/Grade.java
@@ -0,0 +1,29 @@
+package com.lichuang.jdbc.bean;
+
+public class Grade {
+	
+	private String id;
+	private String studentId;
+	private String courseId;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(String courseId) {
+		this.courseId = courseId;
+	}
+	
+	
+
+}
