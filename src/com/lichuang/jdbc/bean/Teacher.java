3465c7cddb4fb2854861b7c4d1a8e1bd5a04a8ad
li-chuang <278747148@qq.com>
2016-11-20 22:42:25 +0800

    Update Teacher.java

--- src/com/lichuang/jdbc/bean/Teacher.java
+++ src/com/lichuang/jdbc/bean/Teacher.java
@@ -9,7 +9,19 @@ public class Teacher {
 	private String age;
 	private String course;//课程方向
 	
+	public Teacher(){
+		
+	}
 	
+	public Teacher(String id, String name, String password, String sex,
+			String age, String course) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.sex = sex;
+		this.age = age;
+		this.course = course;
+	}
 	public String getId() {
 		return id;
 	}
