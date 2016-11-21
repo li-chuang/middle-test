7179bd80a8e58408180bdb840ba3476c8ef3aa24
li-chuang <278747148@qq.com>
2016-11-21 23:44:34 +0800

    Update Grade.java

--- src/com/lichuang/jdbc/bean/Grade.java
+++ src/com/lichuang/jdbc/bean/Grade.java
@@ -1,38 +1,53 @@
 package com.lichuang.jdbc.bean;
 
 public class Grade {
-	
+
 	private String id;
 	private String studentId;
 	private String courseId;
-	
-	public Grade(){
-		
+	private String grade;
+
+	public Grade() {
+
 	}
-	public Grade(String id, String studentId, String courseId) {
+
+	public Grade(String id, String studentId, String courseId, String grade) {
 		this.id = id;
 		this.studentId = studentId;
 		this.courseId = courseId;
+		this.grade = grade;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getStudentId() {
 		return studentId;
 	}
+
 	public void setStudentId(String studentId) {
 		this.studentId = studentId;
 	}
+
 	public String getCourseId() {
 		return courseId;
 	}
+
 	public void setCourseId(String courseId) {
 		this.courseId = courseId;
 	}
-	
-	
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
 
 }
