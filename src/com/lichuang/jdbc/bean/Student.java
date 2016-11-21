4ef70922f6f7cc386bde23d5685b3c1e15dd2442
li-chuang <278747148@qq.com>
2016-11-22 00:40:18 +0800

    Update Student.java

--- src/com/lichuang/jdbc/bean/Student.java
+++ src/com/lichuang/jdbc/bean/Student.java
@@ -2,21 +2,24 @@ package com.lichuang.jdbc.bean;
 
 public class Student {
 
-	private String id;//学号
+	private String id;// 学号
 	private String name;
 	private String password;
 	private String sex;
 	private String birthday;
-	private String department;//院系
-	private String major;//专业
-	private String classes;//年级
-	
-	public Student(){
-		
+	private String department;// 院系
+	private String major;// 专业
+	private String classes;// 年级
+	private String school;
+
+
+	public Student() {
+
 	}
-	
+
 	public Student(String id, String name, String password, String sex,
-			String birthday, String department, String major, String classes) {
+			String birthday, String department, String major, String classes,
+			String school) {
 		this.id = id;
 		this.name = name;
 		this.password = password;
@@ -25,55 +28,79 @@ public class Student {
 		this.department = department;
 		this.major = major;
 		this.classes = classes;
+		this.school = school;
 	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getSex() {
 		return sex;
 	}
+
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
+
 	public String getBirthday() {
 		return birthday;
 	}
+
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+
 	public String getMajor() {
 		return major;
 	}
+
 	public void setMajor(String major) {
 		this.major = major;
 	}
+
 	public String getClasses() {
 		return classes;
 	}
+
 	public void setClasses(String classes) {
 		this.classes = classes;
 	}
-
 
 }
