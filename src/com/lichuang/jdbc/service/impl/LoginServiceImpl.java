910ca4088ed460d55191d0d9b6bac953d85f5ffe
li-chuang <278747148@qq.com>
2016-11-23 23:57:38 +0800

    Create LoginServiceImpl.java

---
+++ src/com/lichuang/jdbc/service/impl/LoginServiceImpl.java
@@ -0,0 +1,27 @@
+package com.lichuang.jdbc.service.impl;
+
+import com.lichuang.jdbc.bean.Manager;
+import com.lichuang.jdbc.service.ILoginService;
+
+public class LoginServiceImpl implements ILoginService {
+
+	@Override
+	public int login(String username, String password) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int regist(Manager manager) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updatePassword(String username, String password,
+			String repassword) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
