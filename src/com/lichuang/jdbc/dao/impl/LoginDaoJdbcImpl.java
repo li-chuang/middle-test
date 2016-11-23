966a731d265862843f5709770e3fce0f7090c8a5
li-chuang <278747148@qq.com>
2016-11-23 23:52:16 +0800

    Create LoginDaoJdbcImpl.java

---
+++ src/com/lichuang/jdbc/dao/impl/LoginDaoJdbcImpl.java
@@ -0,0 +1,52 @@
+package com.lichuang.jdbc.dao.impl;
+
+import com.lichuang.jdbc.bean.Manager;
+import com.lichuang.jdbc.bean.Student;
+import com.lichuang.jdbc.bean.Teacher;
+import com.lichuang.jdbc.dao.ILoginDao;
+
+public class LoginDaoJdbcImpl implements ILoginDao {
+
+	@Override
+	public Student studentLogin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Teacher teacherLogin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Manager managerLogin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int registManager(Manager manager) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateStudentInfo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateTeacherInfo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateManagerInfo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
