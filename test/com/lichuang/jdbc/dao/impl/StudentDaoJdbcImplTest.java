5bee0166dfbc6fb6e739691cd500142addc2685d
li-chuang <278747148@qq.com>
2016-11-29 00:27:19 +0800

    Create StudentDaoJdbcImplTest.java

---
+++ test/com/lichuang/jdbc/dao/impl/StudentDaoJdbcImplTest.java
@@ -0,0 +1,49 @@
+package com.lichuang.jdbc.dao.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentDaoJdbcImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertBatch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteBatch() {
+		fail("Not yet implemented");
+	}
+
+}
