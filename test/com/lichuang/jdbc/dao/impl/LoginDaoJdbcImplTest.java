8d20bfc39f2fbb7d72bad37de824d574a5490ce8
li-chuang <278747148@qq.com>
2016-11-29 00:26:49 +0800

    Create LoginDaoJdbcImplTest.java

---
+++ test/com/lichuang/jdbc/dao/impl/LoginDaoJdbcImplTest.java
@@ -0,0 +1,54 @@
+package com.lichuang.jdbc.dao.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginDaoJdbcImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testStudentLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTeacherLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testManagerLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegistManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateStudentInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateTeacherInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateManagerInfo() {
+		fail("Not yet implemented");
+	}
+
+}
