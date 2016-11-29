6a7d26a276e7bcdd50526578022649bb5c5d771b
li-chuang <278747148@qq.com>
2016-11-29 23:07:06 +0800

    Update StudentDaoJdbcImplTest.java

--- test/com/lichuang/jdbc/dao/impl/StudentDaoJdbcImplTest.java
+++ test/com/lichuang/jdbc/dao/impl/StudentDaoJdbcImplTest.java
@@ -2,14 +2,53 @@ package com.lichuang.jdbc.dao.impl;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.database.QueryDataSet;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.lichuang.jdbc.util.JdbcMySqlUtils;
+
 public class StudentDaoJdbcImplTest {
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
+		IDatabaseConnection connection = null;
+		try {
+			//获得连接
+			Connection conn = JdbcMySqlUtils.getConnection();
+			connection = new DatabaseConnection(conn);
+			
+			//将指定数据库中的的数据备份
+			QueryDataSet backupDataSet = new QueryDataSet(connection);
+			backupDataSet.addTable("t_student");			
+			File file = File.createTempFile("student_bak", ".xml");
+			FlatXmlDataSet.write(backupDataSet, new FileOutputStream(file));
+			
+			//将准备好的文件读入
+			IDataSet dataSet = new FlatXmlDataSet(new File("student_pre.xml"));
+			DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);//先清除后插入
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			if(connection != null){
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	@After
@@ -47,3 +86,4 @@ public class StudentDaoJdbcImplTest {
 	}
 
 }
+
